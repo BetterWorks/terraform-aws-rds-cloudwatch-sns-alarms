70ed910644d54ed7480cabc4e52cb6d3475b029c
Henry He <henry.he@betterworks.com>
2021-06-24 11:10:53 -0600

    fixing ternary

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "sns_topic_arn" {
   description = "The ARN of the SNS topic"
-  value       = var.rds_enabled ? aws_sns_topic.default[0].arn : ""
+  value       = var.enabled ? aws_sns_topic.default[0].arn : ""
 }
 
 output "sns_topic_name" {
   description = "The Name of the SNS topic"
-  value       = var.rds_enabled ? aws_sns_topic.default.name : ""
+  value       = var.enabled ? aws_sns_topic.default[0].name : ""
 }
